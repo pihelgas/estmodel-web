08ec84ab80b7af4a695bf87154896bde6b30564b
Eero Pihelgas <pihelgas@users.noreply.github.com>
2016-12-06 16:14:12 +0200

    Update EstModel

--- src/main/java/ee/klab/water/web/model/PointSource.java
+++ src/main/java/ee/klab/water/web/model/PointSource.java
@@ -14,10 +14,6 @@ public class PointSource implements Serializable {
 
     }
 
-    public PointSource(String id) {
-        this.id = id;
-    }
-
     public String getId() {
         return this.id;
     }
