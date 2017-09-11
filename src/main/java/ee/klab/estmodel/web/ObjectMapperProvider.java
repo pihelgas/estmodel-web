d4278595da42359dab1b4b4ab4d38ddf55df223b
Eero Pihelgas <pihelgas@users.noreply.github.com>
2017-09-11 20:07:24 +0300

    Switch JSON framework

---
+++ src/main/java/ee/klab/estmodel/web/ObjectMapperProvider.java
@@ -0,0 +1,26 @@
+package ee.klab.estmodel.web;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public final class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper defaultObjectMapper = new ObjectMapper()
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
+            .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL)
+            .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
+            .registerModule(new JavaTimeModule());
+
+    @Override
+    public ObjectMapper getContext(final Class<?> type) {
+        return this.defaultObjectMapper;
+    }
+
+}
