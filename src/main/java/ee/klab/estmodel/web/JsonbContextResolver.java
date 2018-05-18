f9dfb0719081f74017c9df7fc1857a62857ef31d
Eero Pihelgas <pihelgas@users.noreply.github.com>
2018-05-18 08:10:23 +0300

    Reconfigure

--- src/main/java/ee/klab/estmodel/web/JsonbContextResolver.java
+++ src/main/java/ee/klab/estmodel/web/JsonbContextResolver.java
@@ -1,35 +1,28 @@
 package ee.klab.estmodel.web;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-@Produces(MediaType.APPLICATION_JSON)
 public final class JsonbContextResolver implements ContextResolver<ObjectMapper> {
 
-    private static final ObjectMapper DEFAULT = new ObjectMapper()
+    private final ObjectMapper defaultObjectMapper = new ObjectMapper()
             .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .enable(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS)
             .enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
-            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
-            .enable(SerializationFeature.INDENT_OUTPUT)
-            .enable(SerializationFeature.USE_EQUALITY_FOR_OBJECT_ID)
             .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
-            .registerModule(new JavaTimeModule())
+            .registerModule(new AfterburnerModule())
             .registerModule(new Jdk8Module());
 
     @Override
     public ObjectMapper getContext(Class<?> type) {
-        return DEFAULT;
+        return this.defaultObjectMapper;
     }
 
 }
